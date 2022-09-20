bcbb1c091a34fb9d832ccec6c52848cbc47724f0
MaxiOrellanadev <maxiorellana.bru@gmail.com>
2022-09-20 18:15:21 -0300

    tp_mvc

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+const port = 3030;
+const mainRouter = require("./routers/main");
+
+app.use(express.static("public"));
+
+app.use("/" ,mainRouter);
+
+
+
+app.listen(3030, () => console.log(`server running in http://localhost:${port}`))
